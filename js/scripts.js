de4a1e17e2f666500a18404e84a4507195604807
Stefano <tepo@hotmail.it>
2021-03-05 16:17:42 +0100

    Verifico quali ingredienti sono selezionati e faccio la somma

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // 1. dichiarazione variabili
 var calculate = document.getElementById('calculate');
 var burgerName = document.getElementById('burger_name');
+var ingredients = document.getElementsByClassName('ingredients');
 
 // 2. evento al click del button
 calculate.addEventListener('click',
@@ -8,6 +9,16 @@ calculate.addEventListener('click',
 
     // 3. verifico se inserito nome burger
     if (burgerName.value != "") {
+
+      // 4. prendo tutti gli ingredienti e controllo quali sono selezionati
+      var totale = 50;
+      for (var i = 0; i < ingredients.length; i++) {
+        if ( ingredients[i].checked ) {
+          // 4a. se selezionato sommo il valore al totale
+          totale += parseInt(ingredients[i].value);
+          console.log(totale);
+        }
+      }
 
     } else {
       alert("Inserisci il nome del panino")
